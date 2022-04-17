0606d19a3cb2c82ec3adf2c204f8354ea62c6030
JezerM <amyuki4@icloud.com>
2022-04-17 17:55:16 -0600

    Added more links to Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,54 @@
 <template>
   <footer id="footer">
-    <b>GitHub repositories</b>
-    <ul>
-      <li>
-        <a href="https://github.com/JezerM/web-greeter">Web Greeter</a>
-      </li>
-      <li>
-        <a href="https://github.com/JezerM/nody-greeter">Nody Greeter</a>
-      </li>
-      <li>
-        <a href="https://github.com/JezerM/sea-greeter">Sea Greeter</a>
-      </li>
-    </ul>
+    <section id="githubRepos">
+      <b>GitHub repositories</b>
+      <ul>
+        <li>
+          <a href="https://github.com/JezerM/web-greeter">Web Greeter</a>
+        </li>
+        <li>
+          <a href="https://github.com/JezerM/nody-greeter">Nody Greeter</a>
+        </li>
+        <li>
+          <a href="https://github.com/JezerM/sea-greeter">Sea Greeter</a>
+        </li>
+        <li>
+          <a href="https://github.com/JezerM/web-greeter-page">This webpage</a>
+        </li>
+      </ul>
+    </section>
+    <section id="githubRepos">
+      <b>Contact</b>
+      <ul>
+        <li>
+          <a href="https://github.com/JezerM/">JezerM's GitHub</a>
+        </li>
+        <li>
+          <a href="mailto:amyuki4@gmail.com">Send Email</a>
+        </li>
+      </ul>
+    </section>
+    <section id="githubRepos">
+      <b>Support me</b>
+      <ul>
+        <li>
+          <a href="https://ko-fi.com/jezerm">Ko-fi</a>
+        </li>
+      </ul>
+    </section>
   </footer>
 </template>
 
 <style>
 #footer {
+  display: flex;
+
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: left;
+  gap: 1em 3em;
+
   font-size: 14px;
   text-align: left;
   color: hsl(0, 0%, 60%);
