11c182dbae83c5f5dd1001dc0cd975fa23be3c11
Jezer Mejía <amyuki4@icloud.com>
2022-06-12 19:09:03 +0000

    Migrated from spa to ssg with vue-ssg

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,32 @@
-import { createApp } from "vue";
+import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
-import router from "./router";
 import "inter-ui/inter.css";
 import "./index.css";
 
-createApp(App).use(router).mount("#app");
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+import Home from "./views/Home.vue";
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/themes",
+    name: "Themes",
+    component: () => import("./views/Themes.vue"),
+  },
+];
+
+//createApp(App).use(router).mount("#app");
+
+export const createApp = ViteSSG(
+  // the root component
+  App,
+  // vue-router options
+  {
+    routes: routes,
+  }
+);
