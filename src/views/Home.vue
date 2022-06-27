52d72e727e67756f88dcb271f7111f515012160f
Jezer Mejía <amyuki4@icloud.com>
2022-06-27 19:57:17 +0000

    Added Projects section with the 3 greeters info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <section id="homeView">
     <Introduction />
+    <Projects />
     <Features />
   </section>
 </template>
@@ -8,11 +9,13 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import Introduction from "@/components/Introduction.vue";
+import Projects from "@/components/Projects.vue";
 import Features from "@/components/Features.vue";
 
 export default defineComponent({
   components: {
     Introduction,
+    Projects,
     Features,
   },
   setup() {},
