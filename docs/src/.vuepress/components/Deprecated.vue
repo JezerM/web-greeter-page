b9bfb51d204f634259b2a4aa2469e88da89964c9
Jezer Mejía <amyuki4@icloud.com>
2022-07-04 13:05:36 -0600

    Added Deprecated, Internal and ReadOnly components for markdown usage

---
+++ docs/src/.vuepress/components/Deprecated.vue
@@ -0,0 +1,23 @@
+<template>
+  <Badge type="danger" :text="deprecatedText" vertical="middle" />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { usePageLang } from "@vuepress/client";
+
+const pageLang = usePageLang();
+
+const deprecatedText = ref("Deprecated");
+
+switch (pageLang.value) {
+  case "en-US":
+    deprecatedText.value = "Deprecated";
+    break;
+  case "es":
+    deprecatedText.value = "Obsoleto";
+    break;
+  default:
+    break;
+}
+</script>
---
+++ docs/src/.vuepress/components/Internal.vue
@@ -0,0 +1,23 @@
+<template>
+  <Badge type="normal" :text="internalText" vertical="middle" />
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { usePageLang } from "@vuepress/client";
+
+const pageLang = usePageLang();
+
+const internalText = ref("Internal");
+
+switch (pageLang.value) {
+  case "en-US":
+    internalText.value = "Internal";
+    break;
+  case "es":
+    internalText.value = "Interno";
+    break;
+  default:
+    break;
+}
+</script>
