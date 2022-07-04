e31d7d5b0c32e1816ff0db2dd00c4192348d84ee
Jezer Mejía <amyuki4@icloud.com>
2022-07-04 17:29:46 -0600

    Added a vercel parameter to change github-pages links to vercel ones

--- replace-base.sh
+++ replace-base.sh
@@ -3,3 +3,8 @@
 sed -i 's/\(\(base\|path\):.*"\)\/web-greeter-page\//\1\//g' docs/src/.vuepress/config.ts src/main.ts
 sed -i 's/\(\(to\|href\)="\)\/web-greeter-page\//\1\//g' src/components/MenuBar.vue
 sed -i 's/\(base:.*"\)\/web-greeter-page\//\1/g' vite.config.ts
+
+if [ "$1" = "vercel" ]; then
+  web_link="https:\/\/web-greeter-page\.vercel\.app\/"
+  sed -i "s/\(link:.*\)\"https:\/\/jezerm\.github\.io\/web-greeter-page\/\"/\1\"${web_link}\"/g" docs/src/.vuepress/config.ts
+fi
